c22c5624c14b8328889672981a3fa89b9fd1e282
lucius <nick_zzhao@outlook.com>
2022-07-21 12:51:20 +0800

    add Dockerfile

--- frontend/src/util/request.ts
+++ frontend/src/util/request.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "./token";
 
 export const request = axios.create({
-  baseURL: "http://1ucius.top:5000/api",
+  baseURL: "http://localhost:5000/api",
   timeout: 6000,
 });
 
